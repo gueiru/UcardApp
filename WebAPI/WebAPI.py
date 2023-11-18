1eb3003e6ddec7d1e2ce95427d78f61182e7555e
marslin1214 <126048638+marslin1214@users.noreply.github.com>
2023-11-19 00:01:36 +0800

    20231118jwt-webapi

--- WebAPI/WebAPI.py
+++ WebAPI/WebAPI.py
@@ -25,7 +25,8 @@ import json
 app = Flask(__name__)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://test:test@localhost:3306/ucard'
-app.config['JWT_SECRET_KEY'] = 'SECRET_KEY' #這裡要更改成想要的密碼
+app.config['JWT_ID_SECRET_KEY'] = 'SECRET_KEY' #這裡要更改成想要的密碼
+app.config['JWT_EMAIL_SECRET_KEY'] = 'SECRET_KEY' #這裡要更改成想要的密碼
 db = SQLAlchemy(app)
 bcrypt = Bcrypt()
 
@@ -102,9 +103,10 @@ class basic(db.Model):
 @app.route('/login', methods=['POST'])
 def login():
   email = request.json['email']
+  jwt_email = jwt.encode({'email':email}, app.config['JWT_EMAIL_SECRET_KEY'], algorithm='HS256')
   password = request.json['password']
   # 檢查是使用者是否存在
-  user = users.query.filter_by(email=email).first()
+  user = users.query.filter_by(email=jwt_email).first()
   if not user:
     return jsonify({'message': '查無此帳號，請重新輸入'}), 404
   # 確認密碼是否吻合
@@ -112,7 +114,7 @@ def login():
     return jsonify({'message': '密碼錯誤'}), 401
   # 登入成功通知
   username = email[:email.index("@")]
-  usertoken = jwt.encode({'user_id': user.user_id}, app.config['JWT_SECRET_KEY'], algorithm='HS256')
+  usertoken = jwt.encode({'user_id': user.user_id}, app.config['JWT_ID_SECRET_KEY'], algorithm='HS256')
 
   return jsonify({'message': 'User logged in successfully','userinf':{'username': username,'email': email,'token': usertoken}}), 200
 
@@ -121,24 +123,26 @@ def register():
   email = request.json['email']
   password = request.json['password']
   hashed_password = bcrypt.generate_password_hash(password=password)
+  jwt_email = jwt.encode({'email':email}, app.config['JWT_EMAIL_SECRET_KEY'], algorithm='HS256')
   # 檢查是否註冊
-  email_check = users.query.filter_by(email=email).first()
+  email_check = users.query.filter_by(email=jwt_email).first()
   if email_check:
     return jsonify({'message': '電子信箱已註冊過'}), 400
   # Create a new user
-  new_user = users(None,email, hashed_password)
+  new_user = users(None, jwt_email, hashed_password)
   db.session.add(new_user)
   db.session.commit()
   username = email[:email.index("@")]
-  user_id = users.query.filter_by(email=email).first().user_id
-  usertoken = jwt.encode({'user_id': user_id}, app.config['JWT_SECRET_KEY'], algorithm='HS256')
+  user_id = users.query.filter_by(email=jwt_email).first().user_id
+  usertoken = jwt.encode({'user_id': user_id}, app.config['JWT_ID_SECRET_KEY'], algorithm='HS256')
 
   return jsonify({'message': 'User registered successfully','userinf':{'username': username,'email': email,'token': usertoken}}), 201
 
 @app.route('/forgetpw', methods=['POST'])
 def forgetpw():
   email = request.json['email']
-  user = users.query.filter_by(email=email).first()
+  jwt_email = jwt.encode({'email':email}, app.config['JWT_EMAIL_SECRET_KEY'], algorithm='HS256')
+  user = users.query.filter_by(email=jwt_email).first()
   if not user:
     return jsonify({'message': '查無此帳號，請重新輸入'}), 404
   else:
@@ -170,20 +174,21 @@ def forgetpw():
         print("Error message: ", e)
   
   password = hashlib.sha256(password.encode('utf-8')).hexdigest()
-  users.query.filter_by(email=email).update({'password': bcrypt.generate_password_hash(password=password)})
+  users.query.filter_by(email=jwt_email).update({'password': bcrypt.generate_password_hash(password=password)})
   db.session.commit()
   return jsonify({'message': 'User change password successfully'}), 202
 
 @app.route('/changepwd', methods=['POST'])
 def changepwd():
   email = request.json['email']
+  jwt_email = jwt.encode({'email':email}, app.config['JWT_EMAIL_SECRET_KEY'], algorithm='HS256')
   password = request.json['password']
   newpassword = request.json['newpassword']
-  user = users.query.filter_by(email=email).first()
+  user = users.query.filter_by(email=jwt_email).first()
   #判斷舊密碼是否正確
   if not bcrypt.check_password_hash(user.password, password):
     return jsonify({'message': '密碼錯誤'}), 401
-  users.query.filter_by(email=email).update({'password': bcrypt.generate_password_hash(password=newpassword)})
+  users.query.filter_by(email=jwt_email).update({'password': bcrypt.generate_password_hash(password=newpassword)})
   db.session.commit()
   username = email[:email.index("@")]
   return jsonify({'message': 'User logged in successfully','userinf':{'username': username,'email': email,'token': user.user_id}}), 203
@@ -191,7 +196,7 @@ def changepwd():
 @app.route('/getbank', methods=['POST'])
 def getbank():
   data = request.json
-  user_id = jwt.decode(data['usertoken'], app.config['JWT_SECRET_KEY'], algorithms=['HS256'])['user_id']
+  user_id = jwt.decode(data['usertoken'], app.config['JWT_ID_SECRET_KEY'], algorithms=['HS256'])['user_id']
   banklist = banks.query.all()
   
   # 陣列化資料
@@ -220,7 +225,7 @@ def getbank():
 @app.route('/add_bank', methods=['POST'])
 def add_bank():
   data = request.json
-  user_id = jwt.decode(data['usertoken'], app.config['JWT_SECRET_KEY'], algorithms=['HS256'])['user_id']
+  user_id = jwt.decode(data['usertoken'], app.config['JWT_ID_SECRET_KEY'], algorithms=['HS256'])['user_id']
   bankdata = data['bank_data']
   user_banklist.query.filter_by(user_id=user_id).delete()
   
@@ -235,7 +240,7 @@ def add_bank():
 @app.route('/getcard', methods=['POST'])
 def getcard():
   bank_id = request.json['bank_id']
-  user_id = jwt.decode(request.json['usertoken'], app.config['JWT_SECRET_KEY'], algorithms=['HS256'])['user_id']
+  user_id = jwt.decode(request.json['usertoken'], app.config['JWT_ID_SECRET_KEY'], algorithms=['HS256'])['user_id']
   cardlist = cards.query.filter_by(bank_id=bank_id).all()
 
   # 陣列化資料
@@ -287,7 +292,7 @@ def getcard():
 @app.route('/add_card', methods=['POST'])
 def add_card():
   data = request.json
-  user_id = jwt.decode(data['usertoken'], app.config['JWT_SECRET_KEY'], algorithms=['HS256'])['user_id']
+  user_id = jwt.decode(data['usertoken'], app.config['JWT_ID_SECRET_KEY'], algorithms=['HS256'])['user_id']
   bank_id = data['bank_id']
   carddata = data['card_list']
   user_cardlist.query.filter_by(user_id=user_id,bank_id=bank_id).delete()
@@ -348,7 +353,7 @@ def getshop():
 def recommend():
   search_keyword = request.json["shopname"]
   amount = request.json["amount"]
-  user_id = jwt.decode(request.json['usertoken'], app.config['JWT_SECRET_KEY'], algorithms=['HS256'])['user_id']
+  user_id = jwt.decode(request.json['usertoken'], app.config['JWT_ID_SECRET_KEY'], algorithms=['HS256'])['user_id']
 
   cardlist = user_cardlist.query.filter_by(user_id = user_id).all()
   serialized_cards = [] # 推薦前的卡片id
@@ -408,4 +413,4 @@ def recommend():
   return jsonify(result),201
 
 if __name__ == '__main__':
-  app.run(debug='true',host='192.168.247.167') #192.168.50.151、192.168.176.197
+  app.run(debug='true',host='192.168.50.151') #192.168.50.151、192.168.176.197
